729769dbe208f51f194ab54f3c5a110f6ecefbc5
Tristan San Juan <tristansj88@gmail.com>
2021-05-11 15:33:09 -0700

    adding sounds

---
+++ react-app/src/components/Carousel/Carousel.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import './Carousel.css'
+
+function Carousel({ photos }) {
+	const [index, setIndex] = useState(0);
+	const max = photos.length - 1;
+	useEffect(() => {}, [index]);
+	return (
+		<div className="Carousel-Container">
+			<button
+				className="previous"
+				onClick={(e) =>
+                    setIndex((prev) => (prev > 0 ? prev - 1 : (prev = max)))}>⏪</button>
+            <button
+				className="next"
+				onClick={(e) =>
+					setIndex((prev) => (prev < max ? prev + 1 : (prev = 0)))}>⏩</button>
+
+
+            
+            <div><img src={photos[index].photo_url} alt="None" className="car-images" />{photos[index].photo_description}</div>
+			
+		</div>
+	);
+}
+
+export default Carousel
--- react-app/src/components/SingleAnimalPage/SingleAnimalPage.js
+++ react-app/src/components/SingleAnimalPage/SingleAnimalPage.js
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router'
 import { getAnimals, singleAnimal} from '../../store/animal'
 import MapComponent from '../MapComponent/MapComponent'
 import ReactAudioPlayer from 'react-audio-player'
-
+import CarouselComponent from '../Carousel/Carousel'
 
 
 function SingleAnimalPage() {
@@ -26,29 +26,34 @@ function SingleAnimalPage() {
         return null
     }        
     return (
-        <>
-            <div>{animal.name}</div>
-            <div>{animal.description}</div>
-            <div>{animal.fact_1}</div>
-            <div>{animal.fact_2}</div>
-            <div>{animal.fact_3}</div>
-            <div>{animal.fact_4}</div>
-            <div>{animal.fact_5}</div>
-            <div>{animal.fact_6}</div>
-            <div>{animal.threats}</div>
-          
-            
-            
-            { animal.locations.map((location) => (
-                <div>
-                    <div>Name:{location.location_name} Lat:{location.lat} Lng:{location.lng}</div>
-                </div>
-            ))}
-            <MapComponent className="map-component" />
-            
+			<>
+				<CarouselComponent photos={animal.photos} />
+				<ReactAudioPlayer
+                src="cd.textfiles.com/mmplus/MEDIA/WAV/EFFECTS/ANIMALS/TIGER.WAV"
+                autoPlay="true"
+                controls="true"
+				/>
+				<div>{animal.name}</div>
+				<div>{animal.description}</div>
+				<div>{animal.fact_1}</div>
+				<div>{animal.fact_2}</div>
+				<div>{animal.fact_3}</div>
+				<div>{animal.fact_4}</div>
+				<div>{animal.fact_5}</div>
+				<div>{animal.fact_6}</div>
+				<div>{animal.threats}</div>
 
-            </>
-    )
+				{animal.locations.map((location) => (
+					<div>
+						<div>
+							Name:{location.location_name} Lat:{location.lat} Lng:
+							{location.lng}
+						</div>
+					</div>
+				))}
+				<MapComponent className="map-component" />
+			</>
+		);
 }
 
 export default SingleAnimalPage;
