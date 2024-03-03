5f540f32ecfc08738bb0b81bbd42fc0e133c6618
Tristan San Juan <tristansj88@gmail.com>
2024-03-02 23:16:15 -0700

    reset5

---
+++ app/models/comment 2.py
@@ -0,0 +1,21 @@
+from app.models.db  import db, environment, SCHEMA
+from sqlalchemy.orm import relationship
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+    
+    id= db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    
+    
+    #associations
+    comment_user = relationship("Comment_Joint", backref="user_comment", cascade="all, delete")
+    comment_animal = relationship("Comment_Joint", backref="animal_comment", cascade="all, delete")
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "content": self.content
+        }
