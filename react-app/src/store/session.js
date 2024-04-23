fe149fcd2bb9738b7b7106fcc98db9c03bfa84ed
Tristan San Juan <tristansj88@gmail.com>
2024-04-23 01:15:05 -0700

    made some lint changes

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -112,7 +112,7 @@ export const logout = () => async (dispatch) => {
             "Content-Type": "application/json",
         }
     });
-    const data = await response.json();
+    await response.json();
     dispatch(removeUser());
 };
 
